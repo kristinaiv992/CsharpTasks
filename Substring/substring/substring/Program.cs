1eccc2ba3e0cbdf54cc2e78a6d82df8f053b538c
Kristina <kristinaiv992@gmail.com>
2019-03-10 22:22:57 +0100

    added a new feature some files changed

---
+++ Substring/substring/substring/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace od_cas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringCutter();
+        }
+
+
+        static void StringCutter()
+        {
+
+            Console.WriteLine("Please enter a sentence?");
+            string sentence = Console.ReadLine();
+            Console.WriteLine(sentence);
+            Console.WriteLine("Please enter a number of letters you want to be cut");
+            int no = int.Parse(Console.ReadLine());
+            var Cutted = sentence.Substring(0, no);
+            var Cutted2 = sentence.Substring(no);
+            Console.WriteLine(Cutted); //displays the cut letters
+            Console.WriteLine(Cutted2); //displays the rest 
+
+        }
+    }
+}
