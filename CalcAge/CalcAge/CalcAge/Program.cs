d3e2c9d279a28294ec02cb15dba59e2e1d979cc4
Kristina Ivanova <45533932+kristinaiv992@users.noreply.github.com>
2019-04-02 15:59:49 +0200

    Update Program.cs

--- CalcAge/CalcAge/CalcAge/Program.cs
+++ CalcAge/CalcAge/CalcAge/Program.cs
@@ -39,6 +39,35 @@ namespace CalcAge
 
 
             //--------------------------------------------------------------------//
+            var Date = new DateTime(2019,04,02);
+            Console.WriteLine(Date);
+            var DateTime = new DateTime(2019, 1, 1);
+            var now = DateTime.Now;
+            var today = DateTime.Today;
+            var tomorrow1 = DateTime.Today.AddDays(1);
+            Console.WriteLine("Hour at the moment" + now.Hour);
+            Console.WriteLine(" Minutes at the moment" + now.Minute);
+
+            var tommorow = now.AddDays(1);
+            var yesterday = today.AddDays(-1);
+
+            Console.WriteLine(now.ToLongDateString());
+            Console.WriteLine(now.ToShortDateString());
+            Console.WriteLine(now.ToLongTimeString());
+            Console.WriteLine(now.ToShortTimeString());
+            Console.WriteLine(now.ToString());
+            Console.WriteLine(now.ToString("yyyy-MM-dd HH:mm"));
+
+            //------Time Span-------------//
+            var TimeSpan = new TimeSpan(1, 2, 3);
+            var TimeSpan1 = new TimeSpan(1, 0, 0);
+            var TimeSpan2 = TimeSpan.FromHours(1);
+            Console.WriteLine("TimeSpan:" + TimeSpan + TimeSpan1 + TimeSpan2);
+            var start = DateTime.Now;
+            var end = DateTime.Now.AddMinutes(2);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
+
         }
     }
 }
